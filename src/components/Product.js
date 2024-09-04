c520c2b76ebec7df84a078c5b168d93f869db326
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-09-04 22:28:43 +0330

    show product and category in home section

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { BsPlus, BsEyeFill } from "react-icons/bs";
 
 const Product = ({ product }) => {
+  console.log(product);
   const { id, image, category, title, price } = product;
   return (
     <div>
@@ -22,12 +23,21 @@ const Product = ({ product }) => {
               <BsPlus className="text-3xl" />
             </div>
           </button>
-          <Link to="/" className="bg-white w-12 h-12 flex items-center justify-center drop-shadow-xl text-primary">
-          <BsEyeFill />
+          <Link
+            to="/product/${id}"
+            className="bg-white w-12 h-12 flex items-center justify-center drop-shadow-xl text-primary"
+          >
+            <BsEyeFill />
           </Link>
         </div>
       </div>
-      <div>2</div>
+      <div>
+        <div className="text-sm capitalize text-gray-500">{category}</div>
+        <Link to="/product/${id}">
+          <h2 className="font-semibold mb-1">{title}</h2>
+        </Link>
+        <div className="font-semibold">$ {price}</div>
+      </div>
     </div>
   );
 };
