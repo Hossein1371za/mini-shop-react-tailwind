29f34c6c7dab5c37de7e463e7f3882f3046972e0
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-09-06 18:06:45 +0330

    cart context

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,8 +4,8 @@ import { BsPlus, BsEyeFill } from "react-icons/bs";
 import { CartContext } from "../contexts/CartContext";
 
 const Product = ({ product }) => {
-const {addTocart} = useContext(CartContext)
-  const {  image, category, title, price } = product;
+const {addToCart} = useContext(CartContext)
+  const { id, image, category, title, price } = product;
   return (
     <div>
       <div className="border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition">
@@ -19,7 +19,7 @@ const {addTocart} = useContext(CartContext)
           </div>
         </div>
         <div className="absolute top-6 -right-11 p-2 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 gap-y-2 transition-all duration-300 group-hover:right-5">
-          <button onClick={addTocart}>
+          <button onClick={()=>addToCart(product,id)}>
             <div className="flex items-center justify-center h-12 w-12 text-white bg-red-500">
               <BsPlus className="text-3xl" />
             </div>
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,16 +1,46 @@
-import React, { createContext,useEffect,useState } from 'react';
+import React, { createContext, useEffect, useState } from "react";
 
-export const CartContext = createContext()
+export const CartContext = createContext();
 
-const CartProvider = ({children}) => {
-const [cart,setCart] = useState([])
-const addTocart = ()=>{
-  console.log("addTocart");
-}
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+  const addToCart = (product, id) => {
+    const newItem = { ...product, amount: 1 };
+    const cartItem = cart.find((item) => {
+      return item.id === id;
+    });
+    if (cartItem) {
+      const newCart = [...cart].map((item) => {
+        if (item.id === id) {
+          return { ...item, amount: cartItem.amount + 1 };
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart)
+    }else{
+      setCart([...cart,newItem])
+    }
+  };
+  // secound 
+  // const addToCart = (id, product) => {
+  //   setCart((prevCart) => {
+  //     const cartItem = prevCart.find((item) => item.id === id);
+  //     if (cartItem) {
+  //       return prevCart.map((item) =>
+  //         item.id === id ? { ...item, amount: item.amount + 1 } : item
+  //       );
+  //     } else {
+  //       return [...prevCart, { ...product, amount: 1 }];
+  //     }
+  //   });
+  // };
 
-  return <CartContext.Provider value={{addTocart}}>
-    {children}
-  </CartContext.Provider>;
+  return (
+    <CartContext.Provider value={{ addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
