7b5dcc8d85fe0c8a388d0c0a5bd7b0b4238ffd5d
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-09-07 18:24:06 +0330

    sidebar item

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { IoMdClose } from "react-icons/io";
+import { IoMdAdd, IoMdClose, IoMdRemove } from "react-icons/io";
 
 const CartItem = ({ item }) => {
   const { id, image, title, amount, price } = item;
 
   return (
-    <div className="flex">
+    <div className="flex gap-x-4 py-2 lg:px-6 border-b border-gr200 w-full font-light text-gray-500 ">
       <div className="w-full min-h-[150px] flex items-center gap-x-4">
         <Link to={`/product/${id}`}>
           <img className="max-w-[80px]" src={image} alt="" />
@@ -24,9 +24,23 @@ const CartItem = ({ item }) => {
             </div>
           </div>
           <div className="flex gap-x-2 h-[36px] text-sm">
-            <div>qty</div>
-            <div>price</div>
-            <div>final price</div>
+            <div className="flex flex-1 max-w-[100px] items-center h-full border text-primary font-medium">
+              <div className="flex-1 flex justify-center items-center h-full cursor-pointer">
+                <IoMdRemove />
+              </div>
+              <div className="flex items-center justify-center px-2">
+                {amount}
+              </div>
+              <div className="flex-1 flex justify-center items-center h-full cursor-pointer">
+                <IoMdAdd />
+              </div>
+            </div>
+            <div className="flex-1 flex justify-around items-center">
+              $ {price}
+            </div>
+            <div className="flex-1 flex justify-end items-center text-primary font-medium">{`$ ${parseFloat(
+              price * amount
+            ).toFixed(2)}`}</div>
           </div>
         </div>
       </div>
