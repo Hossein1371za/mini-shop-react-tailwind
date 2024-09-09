f53d5e827075a67fdc2240bac6d81695bb217595
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-09-09 22:54:49 +0330

    hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React from "react";
+// import WomanImage from "../assets/img/woman_hero.png"
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="bg-hero1 h-[800px] bg-no-repeat bg-cover bg-center py-24"></section>
+  );
 };
 
 export default Hero;
